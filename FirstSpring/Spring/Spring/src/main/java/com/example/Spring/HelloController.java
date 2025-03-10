981e1b3bfe02839c1538c4f8ae028020556f6cd7
Michelle Mimi <mmangoka@gmail.com>
2025-03-10 07:01:15 +0300

    Rest Api examples

--- FirstSpring/Spring/Spring/src/main/java/com/example/Spring/HelloController.java
+++ FirstSpring/Spring/Spring/src/main/java/com/example/Spring/HelloController.java
@@ -1,22 +1,25 @@
 package com.example.Spring;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController//speciliazed version of controller annotation
 public class HelloController {
 
+
+    @GetMapping("/hello/{name}/show")
+    public HelloResponse helloParam(@PathVariable String name){
+        return new HelloResponse("Hello," +  name);
+    }
+
     @GetMapping("/hello")
-    public String hello(){
-        return "Hello World";
+    public HelloResponse hello(){
+        return new HelloResponse("Hello World.");
     }
 
     @PostMapping("/hello")
-    public String helloPost(@RequestBody String name){
-        return "Hello " +  name + "!";
+    public HelloResponse helloPost(@RequestBody String name){
+        return new HelloResponse("Hello " +  name + "!");
     }
 
 }
