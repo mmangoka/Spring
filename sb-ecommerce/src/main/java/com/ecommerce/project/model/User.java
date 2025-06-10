e4671df3fda7505a752831c70eb2e19c91ff83c2
Michelle Mimi <mmangoka@gmail.com>
2025-06-10 07:37:33 +0300

    defined relationships for classes user and roles and added unique constraint attribute

--- sb-ecommerce/src/main/java/com/ecommerce/project/model/User.java
+++ sb-ecommerce/src/main/java/com/ecommerce/project/model/User.java
@@ -5,17 +5,25 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @NoArgsConstructor
 @Data
-@Table(name = "users")
+@Table(name = "users",
+          uniqueConstraints = @UniqueConstraint(columnNames = {"userName","email"})
+)
 public class User {
 
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @NotBlank
@@ -38,5 +46,13 @@ public class User {
         this.password = password;
     }
 
-    private Set<Role>  roles =  new HashSet<>();
+    @Setter
+    @Getter
+    @ManyToMany(cascade = {CascadeType.PERSIST,CascadeType.MERGE},
+    fetch = FetchType.EAGER)
+    @JoinTable(name = "user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles =  new HashSet<>();
+
 }
